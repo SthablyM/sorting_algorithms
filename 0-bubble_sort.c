51be48f1b5d9418e7e13ae5cd15a4922d50187fe
SthablyM <mathapelofentse@icloud.com>
2024-03-04 07:50:53 -0800

    0-bubble_sort.c, 0-O

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,6 +1,6 @@
 # include "sort.h"
 /**
- *swap_int - swap integers in an array
+ *swap - swap integers in an array
  *@xp: first integer to swap
  *@yp: second integer to swap
  */
@@ -32,15 +32,14 @@ void bubble_sort(int *array, size_t size)
 		bubble = false;
 		for (i = 1; i < len; i++)
 		{
-			if (array[i-1] > array[i])
+			if (array[i - 1] > array[i])
 			{
-				temp = array[i-1];
-				array[i-1] = array[i];
-				array[i] = temp;					
+				temp = array[i - 1];
+				array[i - 1] = array[i];
+				array[i] = temp;
 				print_array(array, size);
 				bubble = true;
 			}
 		}
-		
 	}
 }
